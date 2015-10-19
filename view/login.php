aa93d202ba177c549b6901b1e42b9d0c63ab130a
shyamu <neupaneshyamu@gmail.com>
2015-10-19 11:42:34 -0500

    no message

--- view/login.php
+++ view/login.php
@@ -1,38 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Login here</title> 
-    
-    <?php session_start(); ?>
-    <meta charset="utf-8">
-</head>
-    <body>
-    <form action="../controller/login-submit.php" method="post">
+<?php include '../view/top.html'?>
+
+    <form action="../controller/login-submit.php" method="post" class="form-signin">
         <fieldset>
-            <legend> User Login</legend>
+            <h2 class="form-signin-heading">Please sign in</h2>
             
-    User Name: <input type="text" name="username" required placeholder="user name">
-    Password: <input type="password" name="password" required placeholder="password">
-        <input type="submit" value="Login" >
+    <label for="inputEmail" class="sr-only">Email address</label> 
+    <input type="text" name="username" required placeholder="user name" class="form-control"><br>
+    <label for="inputEmail" class="sr-only">Password</label><input type="password" name="password" required placeholder="password" class="form-control" autofocus> <br>
+    <input type="submit" value="Login"  class="btn btn-lg btn-primary btn-block">
                 
-        </fieldset>
+    </fieldset>
     </form>
         
         
-    <form action="../controller/signup-submit.php" method="post" >
-        
-        <fieldset>
-        <legend> User registration</legend>
-            User Name: <input type="text" name="username" required placeholder="user name"><br>
-            Password: <input type="password" name="pass" required placeholder="password" ><br>
-            email: <input type="email" name="email" required placeholder="email address"><br>
-            <input type="submit" value="Submit">
-            
-        </fieldset>
+    <form action="../view/signup-form.php" method="get" >
+        <br>
+        <input type="submit" value="Register User"  class="btn btn-lg btn-primary btn-block">
+     
         
         </form>
-    
+    <br><br><br><hr>
     </body>
-
+<?php include '../view/bottom.html'?>
 
 </html>

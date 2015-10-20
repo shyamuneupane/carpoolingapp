943e5a2b847c187e530dc25cc59d4f1f9a048ce8
basnetpradeep <pradeepkumar.basnet@gmail.com>
2015-10-19 21:40:16 -0500

    validating password --pradeep

--- view/signup-form.php
+++ view/signup-form.php
@@ -2,7 +2,7 @@
 include '../view/top.html'
     ?>
 
-
+<script src="signup.js" type="text/javascript" ></script>
     <br><br>
 
 <div class="modal-dialog">
@@ -11,16 +11,16 @@ include '../view/top.html'
    <h1 class="text-center">Please register for new user</h1>
 </div>
     <div class="modal-body">
-    <form action="../controller/signup-submit.php" method="post" class="form-signin">
+    
         
-       
-            
-   <label for="inputUsername" >User Name</label>
+        <form action="../controller/signup-submit.php" method="post" id="form-signin">
+        
+     <label for="inputUsername" >User Name</label>
     <input type="text" name="username" required placeholder="user name" class="form-control"><br>
-    <label for="inputUsername" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control"><br>
-    <label for="inputUsername" >Re-Password</label><input type="password" name="repassword" required placeholder="re-enter password" class="form-control"><br>
+    <label for="inputUsername" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control" id="password"><br>
+    <label for="inputUsername" >Confirm Password</label><input type="password" name="confirmpassword" required placeholder="confirm password" class="form-control" id="confirmpassword"><br>
     <label for="inputUsername" >Email</label><input type="text" name="email" required placeholder="email" class="form-control"><br>
-    <input type="submit" value="Register" class="btn btn-lg btn-primary btn-block">  
+    <input type="submit" value="Register" class="btn btn-lg btn-primary btn-block" id="register">  
         
     </form>
     </div>   

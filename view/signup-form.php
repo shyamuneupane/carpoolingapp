173348cd61f437d696a55aa2b62e01471bc418c4
shyamu <neupaneshyamu@gmail.com>
2015-10-20 10:19:29 -0500

    error removed

--- view/signup-form.php
+++ view/signup-form.php
@@ -12,9 +12,9 @@
     <?php
 include '../view/top.html'
     ?>
-<<<<<<< HEAD
+
 <br><br>
-=======
+
 
 
 
@@ -22,7 +22,7 @@ include '../view/top.html'
 <script src="signup.js" type="text/javascript" ></script>
 
     <br><br>
->>>>>>> a6c8ff7da6d85913fd94e4158db5def703e5ff8e
+
 
 <div class="modal-dialog">
 <div class="modal-content">
@@ -38,23 +38,20 @@ include '../view/top.html'
         
     <label for="password" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control" id="password"><label id="password-change"></label><br>
         
-<<<<<<< HEAD
+
      <label for="inputUsername" >User Name</label>
     <input type="text" name="username" required placeholder="user name" class="form-control"><br>
     <label for="inputUsername" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control" id="password"><br>
     <label for="inputUsername" >Confirm Password</label><input type="password" name="confirmpassword" required placeholder="confirm password" class="form-control" id="confirmpassword"><br>
     <label for="inputUsername" >Email</label><input type="text" name="email" required placeholder="email" class="form-control"><br>
     <input type="submit" id ="register" value="Register" class="btn btn-lg btn-primary btn-block">  
-=======
-        
-    <label for="repassword" >Re-Password</label><input type="password" name="repassword" required placeholder="re-enter password" class="form-control" id="password-check" /><br>
-        <label for="email">Email</label> <input type="email" name="email" required placeholder="email" class="form-control"><br>
-    <input type="submit" value="Register" class="btn btn-lg btn-primary btn-block">  
 
+        
+   
     
         
   
->>>>>>> a6c8ff7da6d85913fd94e4158db5def703e5ff8e
+
         
     </form>
     </div>   

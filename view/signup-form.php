a18daeeb131a7645022a5863a9b5cada40184bf3
shyamu <neupaneshyamu@gmail.com>
2015-10-20 07:50:31 -0500

    no message

--- view/signup-form.php
+++ view/signup-form.php
@@ -1,7 +1,10 @@
 <?php
 include '../view/top.html'
     ?>
-
+<script>
+    
+}
+</script>
 
     <br><br>
 
@@ -12,21 +15,22 @@ include '../view/top.html'
 </div>
     <div class="modal-body">
     <form action="../controller/signup-submit.php" method="post" class="form-signin">
-        
-       
             
    <label for="inputUsername" >User Name</label>
     <input type="text" name="username" required placeholder="user name" class="form-control"><br>
-    <label for="inputUsername" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control"><br>
-    <label for="inputUsername" >Re-Password</label><input type="password" name="repassword" required placeholder="re-enter password" class="form-control"><br>
-    <label for="inputUsername" >Email</label><input type="text" name="email" required placeholder="email" class="form-control"><br>
+        
+    <label for="password" >Password</label> <input type="password" name="password" required placeholder="password" class="form-control" id="password"><label id="password-change"></label><br>
+        
+        
+    <label for="repassword" >Re-Password</label><input type="password" name="repassword" required placeholder="re-enter password" class="form-control" id="password-check" /><br>
+        <label for="email">Email</label> <input type="email" name="email" required placeholder="email" class="form-control"><br>
     <input type="submit" value="Register" class="btn btn-lg btn-primary btn-block">  
         
     </form>
     </div>   
     </div>
 </div>
-   <!--<form action="../controller/signup-submit.php" method="post" >-->
+   
         
     
        <?php include '../view/bottom.html' ?>
